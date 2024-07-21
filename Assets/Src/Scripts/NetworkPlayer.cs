458bd5232089c2da1517c64aaf3cd8f2030c877e
Alexandre <alexandrecaila26@gmail.com>
2024-07-21 14:31:09 +0200

    WIP - input + online movement with prediction

---
+++ Assets/Src/Scripts/NetworkPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Unity.Netcode;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NetworkPlayer : NetworkBehaviour {
+    [SerializeField] private PlayerController _controller;
+
+
+    public override void OnNetworkSpawn() {
+        if (this.IsLocalPlayer == false) {
+            this._controller.Simulated();
+        } else {
+            this._controller.Played();
+        }
+    }
+}
---
+++ Assets/Src/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    private enum Type_e {
+        None,
+        Played,
+        Simulated
+    }
+
+    [SerializeField] private PlayerInput _input;
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private PlayerMovement _movements;
+
+    private Type_e _type;
+
+    private InputAction _moveAction;
+    private InputAction _lookAtAction;
+
+    private void Awake() {
+        InputActionMap actionMap = this._input.actions.FindActionMap("Player");
+        this._moveAction = actionMap.FindAction("Move");
+        this._lookAtAction = actionMap.FindAction("Look");
+    }
+
+    private void Update() {
+        if (this._type == Type_e.Played) {
+            Vector2 moveDirection = this._moveAction.ReadValue<Vector2>();
+            Vector2 lookDirection = this._lookAtAction.ReadValue<Vector2>();
+            this._movements.LocalMovement(moveDirection, lookDirection);
+        } else if (this._type == Type_e.Simulated) {
+            this._movements.SimulateMovement();
+        }
+    }
+
+    public void Simulated() {
+        this._type = Type_e.Simulated;
+        this._input.DeactivateInput();
+        this._controller.enabled = false;
+    }
+
+    public void Played() {
+        this._type = Type_e.Played;
+        this._input.ActivateInput();
+    }
+
+}
