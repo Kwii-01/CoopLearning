75a827c8201d58dc675ce734c214b5b04ede65f1
Alexandre <alexandrecaila26@gmail.com>
2024-07-27 14:29:16 +0200

    ADD - camera setup

--- Assets/Src/Scripts/NetworkPlayer.cs
+++ Assets/Src/Scripts/NetworkPlayer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using Cinemachine;
+
 using Unity.Netcode;
 
 using UnityEngine;
@@ -8,9 +10,13 @@ using UnityEngine.InputSystem;
 
 public class NetworkPlayer : NetworkBehaviour {
     [SerializeField] private PlayerController _controller;
+    [SerializeField] private CinemachineVirtualCamera _vcam;
 
 
     public override void OnNetworkSpawn() {
+        if (this.IsLocalPlayer == false) {
+            this._vcam.Priority = -10;
+        }
         if (this.IsLocalPlayer == false && this.IsHost == false) {
             this._controller.Simulated();
         } else {
